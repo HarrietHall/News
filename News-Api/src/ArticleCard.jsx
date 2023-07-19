1fe2432ed89f270553e506ae3f6bcc873012f653
Harriet <harriethall@github.com[200~git config --global user.email you@example.comgit config --global user.email harriethall@github.com>
2023-07-19 18:22:14 +0100

    add message to inform users to login

--- News-Api/src/ArticleCard.jsx
+++ News-Api/src/ArticleCard.jsx
@@ -1,12 +1,15 @@
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext} from "react";
 import { getArticleById, patchArticleVotes } from "./api.js";
 import { Link } from "react-router-dom";
 import "./ArticleCard.css";
 import { dateFormatter } from "./dateUtils.js";
+import { UserContext } from "./UseContext.jsx";
+
 
 const ArticleCard = ({}) => {
-  const { article_id } = useParams();
+    const { article_id } = useParams();
+    const { user, setUser } = useContext(UserContext);
   const [isLoading, setIsLoading] = useState(true);
   const [articleData, setArticleData] = useState({});
   const [likeVotes, setLikeVotes] = useState(0);
@@ -25,7 +28,7 @@ const ArticleCard = ({}) => {
   }, []);
 
 
-  const handleClick = () => {
+  const handleLikeClick = () => {
 
    
     setLikeVotes((currentLikeVotes) => {
@@ -64,36 +67,60 @@ const ArticleCard = ({}) => {
       });
     }
   
-  
+    if (isLoading) return <p>Loading...</p>;
   
 
   return (
     <section className="articleCard">
       <h1>{articleData.title}</h1>
       <h2>
-        Posted by {articleData.author} on{" "}
-        {dateFormatter(articleData.created_at)}
+        Posted by {articleData.author} on {dateFormatter(articleData.created_at)}
       </h2>
       <p>Topic: {articleData.topic}</p>
       <p>{articleData.body}</p>
-      <button
+      <div id="buttonContainer">
+      {user ? (<button
         aria-label="like this comment"
-        onClick={handleClick}
+        onClick={handleLikeClick}
         disabled={likeVotes > 0}
       >
         👍{articleData.votes + likeVotes}
       </button>
-
-      {likeError ? <p>Something went wrong! Please try again</p> : null}
+      ):(
+        <div>
       <button
+        aria-label="like this comment"
+        onClick={handleLikeClick}
+        disabled={!user}
+        > 
+        👍{articleData.votes + likeVotes}
+      </button>
+            </div>
+      )}
+       {likeError ? <p>Something went wrong! Please try again</p> : null}
+
+      {user ? (<button
         aria-label="dislike this comment"
         onClick={handleDislikeClick}
-        disabled={dislikeVotes < 0}
-      >
+        disabled={dislikeVotes < 0 } 
+        >
         👎{articleData.votes + dislikeVotes}
-      </button>
-
+        </button>
+      ):(
+        <div>
+        <button
+          aria-label="dislike this comment"
+          onClick={handleDislikeClick}
+          disabled={!user}
+          > 
+           👎{articleData.votes + dislikeVotes}
+        </button>
+        
+        </div>
+      )} 
+      </div >
       {dislikeError ? <p>Something went wrong! Please try again</p> : null}
+      <br />
       <Link
         className="link_to_comments"
         key={article_id}
@@ -106,12 +133,16 @@ const ArticleCard = ({}) => {
             ? "View comment"
             : `View all ${articleData.comment_count} comments`}
         </button>
-      </Link>
+        {!user  &&(
+          <p id="loginWarning">Must login to like article</p>
+        )}
+      </Link> 
+      <br />
       <img
         src={articleData.article_img_url}
         alt={`relating to ${articleData.topic}`}
-      />
-    </section>
+        />
+        </section>
   );
 };
 
