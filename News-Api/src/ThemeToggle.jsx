8234d1a9f93ba34476efeff649bfb6468eb4dede
Harriet <harriethall@github.com[200~git config --global user.email you@example.comgit config --global user.email harriethall@github.com>
2023-07-30 20:18:06 +0100

    add useEffect to themetoggle.jsx

--- News-Api/src/ThemeToggle.jsx
+++ News-Api/src/ThemeToggle.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { ThemeContext } from './ThemeContext.jsx';
 
 const ToggleTheme = () => {
@@ -10,20 +10,26 @@ const ToggleTheme = () => {
       return currTheme === 'light' ? 'dark' : 'light';
   
           })
-
- 
-    const color = theme === "light" ? "	#202020" : "#FFF";
-    const backgroundColor = theme === "light" ? "#FFF" : "#202020";
-  
-    document.body.style.color = color;
-    document.body.style.backgroundColor = backgroundColor;
-  
+          
+          
+          const color = theme === "light" ? "	#202020" : "#FFF";
+          const backgroundColor = theme === "light" ? "#FFF" : "#202020";
+          
+          document.body.style.color = color;
+          document.body.style.backgroundColor = backgroundColor;
+          
         }
+        useEffect(() => {
+        const linkColor = theme === 'light' ?  '#FFF':'#000000';
+        document.documentElement.style.setProperty('--link-color', linkColor);
+      }, [theme]);
 
    return (
+ 
     <button onClick={toggleTheme} className={`button__${theme}`}>
     {`${theme}`} mode
     </button>
+ 
     
   );
 };
