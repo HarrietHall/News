6bcb96e4114149054754c0ac5b1c842d693d84f5
Harriet <harriethall@github.com[200~git config --global user.email you@example.comgit config --global user.email harriethall@github.com>
2023-07-30 20:14:27 +0100

    remove label title from articlelistcard

--- News-Api/src/ArticleListCard.jsx
+++ News-Api/src/ArticleListCard.jsx
@@ -6,11 +6,12 @@ const ArticleListCard = ({title, author, topic, comment_count, created_at}) => {
     return (
   
    <section className="articleListCard" >
-           <h2>Title: {title}</h2>
+
+           <h2>{title}</h2>
         <p>Posted by {author} on {created_at}</p>
         <p>Topic: {topic}</p>
         <p>{comment_count} comments</p>
-        
+    
    </section>
     );
   };
